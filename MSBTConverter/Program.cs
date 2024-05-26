e55307dd445a53208c89272f28ba84fb6ac4ad04
KillzXGaming <millernathan35@gmail.com>
2024-05-26 10:25:41 -0400

    Add files.

---
+++ MSBTConverter/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using CLMS;
+
+namespace MSBTConverter
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //first search for any message project files
+            MSBP message_project = null;
+            foreach (var file in Directory.GetFiles("MessageProject"))
+            {
+                if (file.EndsWith("msbp"))
+                    message_project = new MSBP(File.OpenRead(file), false);
+            }
+
+            foreach (string arg in args)
+            {
+                if (arg.EndsWith("msbt"))
+                {
+                    MSBT msbt = new MSBT(File.OpenRead(arg), false);
+                    File.WriteAllText($"{arg}.yaml", msbt.ToYaml(message_project));
+                }
+                if (arg.EndsWith("msbp"))
+                {
+                    MSBP msbp = new MSBP(File.OpenRead(arg), false);
+                    File.WriteAllText($"{arg}.yaml", msbp.ToYaml());
+                }
+                if (arg.EndsWith("yaml"))
+                {
+                    MSBT msbt = MSBT.FromYaml(File.ReadAllText(arg), message_project);
+                    File.WriteAllBytes($"{Path.GetFileNameWithoutExtension(arg)}NEW.msbt", msbt.Save());
+                }
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# MSBTConverter
-A tool to convert MSBT to yaml and back.
+
+# MSBT Converter
+
+A msbt converter that can convert msbt to yaml and back.
+The main purpose of this tool is to support additional msbt features other tools lack.
+This tool includes attributes, tags, msbp labeling all thanks to the CLMS library.
+
+## Usage
+
+Before using, I suggest putting the game's .msbp file in the MessageProject directory.
+This will label all attributes and tag info, which will make it much more readable.
+
+Drag/drop a .msbt to the tool to turn into yaml. 
+Drag/drop the yaml back to create a new msbt.
+
+## Credits
+* [Trippixyz](https://github.com/Trippixyz): Project Leader, Programmer, Tester, Github Manager
+* [KillzXGaming](https://github.com/KillzXGaming): Implementation of other msbt types
+* [Kinnay](https://github.com/kinnay): [Reversing most of the files](https://github.com/Kinnay/Nintendo-File-Formats/wiki/LMS-File-Format)
+* Ray Koopa: Syroot Library Developer
+* [Jenrikku](https://github.com/Jenrikku): Help with setting up a C# library, Motivation on pushing this to Github finally
